b0b4f4dc455baa821f1ff8d478b674d6f39c6ae4
suches <suches@btbtd.com>
2018-10-21 22:21:02 +0800

    up

--- dist/3d/dount/index.js
+++ dist/3d/dount/index.js
@@ -216,6 +216,29 @@ var Dount = function (_VisChartBase) {
             });
 
             if (this.isDone) {
+                /*
+                // create a canvas element
+                var canvas1 = document.createElement('canvas');
+                var context1 = canvas1.getContext('2d');
+                context1.font = "Bold 40px Arial";
+                context1.fillStyle = "rgba(255,255,255,1)";
+                context1.fillText('Hello, world!', 0, 25);
+                
+                // canvas contents will be used for a texture
+                var texture1 = new THREE.Texture(canvas1) 
+                texture1.needsUpdate = true;
+                  
+                var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
+                material1.transparent = true;
+                 console.log( canvas1.width, canvas1.height );
+                 var mesh1 = new THREE.Mesh(
+                    new THREE.PlaneGeometry(canvas1.width, canvas1.height),
+                    material1
+                  );
+                mesh1.position.set(0,0,0);
+                this.scene.add( mesh1 );
+                */
+
                 window.requestAnimationFrame(function () {
                     _this3.animationLine();
                 });
